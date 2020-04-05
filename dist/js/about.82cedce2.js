80d3d80be78b00e2239113887256bb69da13d896
Matthew <mfarmer5102@gmail.com>
2020-04-05 00:11:54 -0400

    Initial dist subtree commit

---
+++ dist/js/about.82cedce2.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},r=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("p",[t._v("©2020 Matthew Farmer")])])}],s=n("2877"),u={},c=Object(s["a"])(u,a,r,!1,null,null,null);e["default"]=c.exports}}]);
+//# sourceMappingURL=about.82cedce2.js.map
